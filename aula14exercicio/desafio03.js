d43f0aa22479bd04de5bf0211347fac3122c0e57
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-05-21 01:14:42 -0300

    desafio03 concluído

--- aula14exercicio/desafio03.js
+++ aula14exercicio/desafio03.js
@@ -1,14 +1,35 @@
-function contador () {
-    var i = document.getElementById('inicio')
-    var f = document.getElementById('fim')
-    var p = document.getElementById('passo')
-    var ini = Number(inicio.value)
-    var fi = Number(fim.value)
-    var pas = Number(passo.value)
-    var r = ini + pas
-    var c = document.getElementById('contador')
-    do {
-        c.innerHTML = `> ${r}`
-    }  while(ini <= fi)
+function contar() {
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let contador = document.getElementById('contador')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //alert('[ERRO] Faltam dados!')
+        contador.innerHTML = 'Impossível contar!'
+    }else {
+        contador.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p){
+                contador.innerHTML += ` ${c} \u{1F449} `
+            }
+            
+        }else {
+            //contagem regressiva
+            for(let c = i; c >= f; c-= p) {
+               contador.innerHTML += ` ${c} \u{1F449}` 
+            }
+        }
+        contador.innerHTML += `\u{1F3C1}`       
+    }
+}  
+    
          
-}
